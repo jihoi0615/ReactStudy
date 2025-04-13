bb016c4cf3e34f6d8b2855fa5b111eadb9de3dce
jihoi0615 <pjhalice0615@gmail.com>
2025-04-13 17:37:07 +0900

    Navbar component 분리

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,20 +3,12 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div className="Page">
-      <header>
-        <h1>메뉴목록</h1>
-      </header>
-      <main>여기에 메인 컨텐츠를 담는다.</main>
-      <footer>
-        <nav className="Navbar">
-          <a className="active" href="#">
-            메뉴목록
-          </a>
-          <a href="#">주문내역</a>
-        </nav>
-      </footer>
-    </div>
+    <nav className="Navbar">
+      <a className="active" href="#">
+        메뉴목록
+      </a>
+      <a href="#">주문내역</a>
+    </nav>
   );
 };
 
