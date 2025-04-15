c7b9f187eb9b4906fadeef358108641d0d9da526
jihoi0615 <pjhalice0615@gmail.com>
2025-04-15 22:33:47 +0900

    버튼 로직 생성, fakedata로 product list 생성하기

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,39 @@ import Title from "./components/Title";
 import ProductItem from "./components/Productitem";
 import Button from "./components/Button";
 
+const fakeProducts = [
+  {
+    id: "CACDA421",
+    name: "해물 계란 라면",
+    price: "6000",
+    thumbnail: "./images/menu-해물계란라면.jpg",
+  },
+  {
+    id: "CACDA422",
+    name: "고소한 바질 파스타",
+    price: "7000",
+    thumbnail: "./images/menu-고소한바질파스타.jpg",
+  },
+  {
+    id: "CACDA423",
+    name: "치즈버거",
+    price: "5500",
+    thumbnail: "./images/menu-치즈버거.jpg",
+  },
+  {
+    id: "CACDA424",
+    name: "후라이드 치킨",
+    price: "12000",
+    thumbnail: "./images/menu-후라이드치킨.jpg",
+  },
+  {
+    id: "CACDA425",
+    name: "불고기 비빔밥",
+    price: "8000",
+    thumbnail: "./images/menu-불고기비빔밥.jpg",
+  },
+];
+
 const App = () => {
   return (
     <div className="ProductPage">
@@ -14,10 +47,12 @@ const App = () => {
         <main>
           <ul>
             <li>
-              <ProductItem name="고소한 바질 파스타" price="6,000원"></ProductItem>
-              <ProductItem name="김밥이 고소한 천국" price="6,000원"></ProductItem>
-              <Button styleType="brand">주문하기</Button>
-              <Button styleType="brand-solid">주문하기</Button>
+              {fakeProducts.map((product) => (
+                <li key={product.id}>
+                  <ProductItem product={product} />
+                </li>
+              ))}
+              ;
             </li>
           </ul>
         </main>
